24c38dc79ab15cf2bda3990ebdb029aaedfd5073
mriggleman <119722213+mriggleman@users.noreply.github.com>
2023-04-13 21:43:03 -0700

    Create WhiskeaseAppApp.swift

---
+++ WhiskeaseAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Story_WhiskeaseApp.swift
+//  Whiskease
+//
+//  Created by Mark Riggleman on 4/8/23.
+//
+
+import SwiftUI
+
+@main
+struct WhiskeyEaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
